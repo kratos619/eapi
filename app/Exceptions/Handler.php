fa1cf3411969a1253e85e6cb98a09b5d49b40aee
kratos619 <gaurav.palaspagar@hotmail.com>
2019-02-19 16:10:01 +0530

    Exception Handlinkg

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -48,6 +50,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+                $data = ["error" => "model not Found"];
+                return response()->json($data, 404);
+            }
+            
+            if ($exception instanceof NotFoundHttpException) {
+                $data = ["error" => "Not Valid Link"];
+                return response()->json($data, 404);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
